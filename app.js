8ed1b4d984e32bbd72f57ca6a87bb83758c825dc
Victoria <vickimarji@icloud.com>
2022-07-13 22:24:12 -0400

    new push

---
+++ app.js
@@ -0,0 +1,25 @@
+var blue = 1;
+var red = 2;
+var green = 3;
+var yellow = 4;
+var purple = 5;
+var brown = 6;
+var orange = 7;
+var black = 8;
+var white = 9;
+var gold = 10;
+
+
+console.log(blue + red);
+console.log(black + gold);
+console.log(yellow - black)
+console.log(purple * orange);
+console.log(white * black);
+console.log(brown / yellow);
+console.log(green / blue);
+
+
+blue++;
+black++;
+red--;
+orange--;
